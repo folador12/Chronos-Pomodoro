dd55a9cb7e021c85c856095f10565302f5a91e04
folador12 <luizantoniofoladorfilho@gmail.com>
2025-12-30 23:32:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex items-center justify-center h-screen w-screen bg-black text-white'>
+      <span className='text-2xl font-bold'>Chronos Pomodoro</span>
+    </div>
+  );
+}
+
+export default App;
